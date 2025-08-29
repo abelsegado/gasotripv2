a224fbf69502d775af997b3ce39d9919230afbcd
abelsegado <=>
2025-08-29 20:39:30 +0200

    gasotrip

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  kilometros: number = 0;
+  consumoMedio: number = 0;
+  precioGasolina: number = 0;
+  integrantes: number = 1;
+
+  litrosConsumidos: number = 0;
+  costeTotal: number = 0;
+  costePorPersona: number = 0;
+  resultadoMostrado: boolean = false;
+
+  constructor() { }
+
+  calcularGasolina() {
+    if (
+      this.kilometros > 0 &&
+      this.consumoMedio > 0 &&
+      this.precioGasolina > 0 &&
+      this.integrantes > 0
+    ) {
+      this.litrosConsumidos = (this.kilometros * this.consumoMedio) / 100;
+      this.costeTotal = this.litrosConsumidos * this.precioGasolina;
+      this.costePorPersona = this.costeTotal / this.integrantes;
+      this.resultadoMostrado = true;
+    } else {
+      this.resultadoMostrado = false;
+    }
+  }
 
 }
